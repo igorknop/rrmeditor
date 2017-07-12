39cc76e16481ef297b576cc830298504c2f6c03a
Igor Knop <igorknop@gmail.com>
2017-07-12 01:28:38 -0300

    Add second instrument check

--- rrmeditor.js
+++ rrmeditor.js
@@ -1,45 +1,55 @@
 var srcElement = null;
 var rrm = {
   bg: 0,
-  bgColors: ["blue","green","yellow", "red", "grey"],
-  in:[
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0]
+  bgColors: ["blue", "green", "yellow", "red", "grey"],
+  in: [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0]
   ],
-  inColors:[
-    ["","MicCla.png","MicEsc.png"],
-    ["","GuitaCla.png","GuitaEsc.png"],
-    ["","BassCla.png","BassEsc.png"],
-    ["","DrumCla.png","DrumEsc.png"]
-  ]
+  inColors: [
+    ["", "MicCla.png", "MicEsc.png"],
+    ["", "GuitaCla.png", "GuitaEsc.png"],
+    ["", "BassCla.png", "BassEsc.png"],
+    ["", "DrumCla.png", "DrumEsc.png"]
+  ],
+  toggle: function(x) {
+    var r = Math.floor((x - 1) / 2);
+    var c = (x - 1) % 2;
+    rrm.in[r][c] += (rrm.in[r][c] < 2) ? 1 : -2;
+    if((c == 1) && (rrm.in[r][c]>0)){
+      for(var i=0;i<4;i++){
+        if(i==r) continue;
+        rrm.in[i][1] = 0;
+      }
+    }
+  }
 }
 document.addEventListener("DOMContentLoaded", function() {
   var card = document.querySelector("section");
-  card.addEventListener("click", function(){
-    rrm.bg += (rrm.bg<3)?1:-3;
+  card.addEventListener("click", function() {
+    rrm.bg += (rrm.bg < 3) ? 1 : -3;
     card.style.backgroundColor = rrm.bgColors[rrm.bg];
   }, false);
 
   var instruments = document.querySelectorAll("section > div");
-  console.log(instruments);
-  for(var i =1; i<9; i++){
+  for (var i = 1; i < 9; i++) {
     instruments[i].addEventListener("click", (function(x) {
-      return function(e){
+      return function(e) {
         e.stopPropagation();
-        toggle(x);
-        var line = Math.floor((x-1)/2);
-        var col = rrm.in[line][(x-1)%2];
-        this.style.backgroundImage = "url(images/"+rrm.inColors[line][col]+")";
+        rrm.toggle(x);
+        updateImages();
       };
     })(i), false);
   }
 });
 
-function toggle(x){
-  var row = Math.floor((x-1)/2);
-  var col = (x-1)%2;
-  console.log(row, col, rrm.in);
-  rrm.in[row][col]+=(rrm.in[row][col]<2)?1:-2;
+function updateImages(){
+  var instruments = document.querySelectorAll("section > div");
+  for(j=1;j<9;j++){
+    var r = Math.floor((j - 1) / 2);
+    var c = rrm.in[r][(j - 1) % 2];
+    instruments[j].style.backgroundImage = "url(images/" + rrm.inColors[r][c] + ")";
+  }
 }
